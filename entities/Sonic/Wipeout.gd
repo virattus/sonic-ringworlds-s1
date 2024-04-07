0ca5d8a23227bac4d6a4f395fb055334bdc22558
virattus <146156039+virattus@users.noreply.github.com>
2024-04-06 23:00:01 -0600

    wipeout works better but does not follow ground

--- entities/Sonic/Wipeout.gd
+++ entities/Sonic/Wipeout.gd
@@ -3,21 +3,24 @@ extends BasicState
 
 
 func Enter(_msg := {}) -> void:
-	owner.AnimTree.set("eparameters/Movement/blend_amount", 0.0)
+	owner.AnimTree.set("parameters/Movement/blend_amount", 0.0)
 	owner.AnimTree.set("parameters/Ground/blend_amount", 1.0)
 	owner.AnimTree.set("parameters/GroundSecondary/blend_amount", 0.0)
 	
 	owner.SndSkid.play()
 	
-	if _msg.has("UpDir"):
-		owner.up_direction = _msg["UpDir"]
-	else:
-		owner.up_direction = owner.FloorNormal
+	owner.up_direction = owner.FloorNormal
+	owner.CharMesh.look_at(owner.global_position + owner.velocity.normalized())
 	
-	if owner.Speed <= 0.0:
-		owner.SetVelocity(owner.CharMesh.GetForwardVector())
-		
-	owner.SetVelocity(owner.velocity.normalized() * owner.PARAMETERS.WIPEOUT_MIN_SLIDE_SPEED)
+	var curSpeed = owner.Speed
+	owner.SetVelocity(owner.CharMesh.GetForwardVector() * curSpeed)
+	
+	if curSpeed <= owner.PARAMETERS.WIPEOUT_MIN_SLIDE_SPEED:
+		owner.SetVelocity(owner.CharMesh.GetForwardVector() * owner.PARAMETERS.WIPEOUT_MIN_SLIDE_SPEED)
+	
+	owner.CharMesh.look_at(owner.global_position + owner.velocity.normalized())
+	owner.CharMesh.AlignToY(owner.FloorNormal)
+
 
 
 func Exit() -> void:
@@ -30,6 +33,6 @@ func Update(_delta: float) -> void:
 	owner.Move(owner.velocity)
 	owner.CharMesh.look_at(owner.global_position + owner.velocity)
 	
-	if owner.Speed <= owner.PARAMETERS.WIPEOUT_MIN_SLIDE_SPEED:
+	if owner.Speed <= owner.PARAMETERS.WIPEOUT_MIN_SPEED:
 		ChangeState("Idle")
 		return
