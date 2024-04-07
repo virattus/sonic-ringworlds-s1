0ce96bad414b162f6175efec2e182e72cb5e36d7
virattus <146156039+virattus@users.noreply.github.com>
2024-04-06 20:48:03 -0600

    added lines to the collsiion indicator

--- entities/Collision/Collision.gd
+++ entities/Collision/Collision.gd
@@ -4,7 +4,14 @@ extends Node3D
 
 func SetToCollision(_collision: KinematicCollision3D) -> void:
 	$CollisionPoint.global_position = _collision.get_position()
-	$CollisionNormal.global_position = _collision.get_position() + (_collision.get_normal() * 0.5)
+	$CollisionNormal.mesh = ImmediateMesh.new()
+	$CollisionNormal.mesh.surface_begin(Mesh.PRIMITIVE_LINES, preload("res://entities/Collision/new_standard_material_3d.tres"))
+	$CollisionNormal.mesh.surface_set_color(Color.RED)
+	$CollisionNormal.mesh.surface_add_vertex(_collision.get_position())
+	$CollisionNormal.mesh.surface_add_vertex(_collision.get_position() + _collision.get_normal())
+	$CollisionNormal.mesh.surface_end()
+	$CollisionNormalCap.global_position = _collision.get_position() + _collision.get_normal()
+	
 	#$CollisionNormal.look_at(_collision.get_normal())
 	#print(_collision.get_normal())
 
