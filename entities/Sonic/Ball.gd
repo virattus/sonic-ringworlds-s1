87dc0c7a66e4c32b5ec5cd5c3ce1c7bc1fbd128f
virattus <146156039+virattus@users.noreply.github.com>
2024-04-15 21:55:48 -0600

    turns out godot lets you set proxies for materials, who knew

--- entities/Sonic/Ball.gd
+++ entities/Sonic/Ball.gd
@@ -3,9 +3,20 @@ extends BasicState
 
 var VerticalVelocity := 0.0
 
+var IsOnFloor := true
+
+var LastFramePosition := Vector3.ZERO
+var LastFramePositionCount := 0.0
+
+
+const LASTFRAMEPOS_DIST_MAX = 0.05
+const LASTFRAMEPOSCOUNT_MAX = 1.0
+
 
 func _ready() -> void:
 	DebugMenu.AddMonitor(self, "VerticalVelocity")
+	DebugMenu.AddMonitor(self, "LastFramePosition")
+	DebugMenu.AddMonitor(self, "LastFramePositionCount")
 
 
 func Enter(_msg := {}) -> void:
@@ -15,6 +26,11 @@ func Enter(_msg := {}) -> void:
 	owner.SndSpinCharge.play()
 	
 	VerticalVelocity = 0.0
+	
+	LastFramePosition = owner.global_position
+	LastFramePositionCount = 0
+	
+	IsOnFloor = owner.is_on_floor()
 
 
 func Exit() -> void:
@@ -32,11 +48,22 @@ func Update(_delta: float) -> void:
 	
 	owner.Move(owner.velocity + (Vector3.UP * VerticalVelocity))
 	
+	if owner.global_position.distance_to(LastFramePosition) < LASTFRAMEPOS_DIST_MAX:
+		LastFramePositionCount += _delta
+	else:
+		LastFramePositionCount = 0.0
+	
+	LastFramePosition = owner.global_position
 	
 	var VerticalModifier := 0.5
 	
 	if owner.is_on_floor():
+		if !IsOnFloor:
+			print("Ball: Hit Ground")
+			IsOnFloor = !IsOnFloor
+		
 		owner.FloorNormal = owner.get_floor_normal()
+		owner.up_direction = owner.FloorNormal #owner.up_direction.slerp(owner.FloorNormal, 2.0 * _delta)
 		
 		var Dot = owner.FloorNormal.dot(Vector3.DOWN)
 		#print(Dot)
@@ -50,8 +77,14 @@ func Update(_delta: float) -> void:
 				VerticalModifier = 0.0
 		else:
 			VerticalModifier = (Dot + 1.0) * 0.5
+	else:
+		if IsOnFloor:
+			print("Ball: Left ground")
+			IsOnFloor = !IsOnFloor
 	
 	#print(VerticalModifier)
 	VerticalVelocity -= owner.PARAMETERS.GRAVITY * _delta * VerticalModifier
-		
-	owner.up_direction = owner.up_direction.slerp(owner.FloorNormal, _delta)
+
+	if LastFramePositionCount > LASTFRAMEPOSCOUNT_MAX:
+		ChangeState("Idle")
+	
