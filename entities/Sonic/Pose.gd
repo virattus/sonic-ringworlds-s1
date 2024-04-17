3b7ce3dc4cc3a236538e0beac31227a8449923d2
virattus <146156039+virattus@users.noreply.github.com>
2024-04-16 20:49:50 -0600

    resort island doubled sided

--- entities/Sonic/Pose.gd
+++ entities/Sonic/Pose.gd
@@ -6,6 +6,8 @@ extends BasicState
 func Enter(_msg := {}) -> void:
 	owner.AnimTree.set("parameters/Movement/blend_amount", 1.0)
 	owner.AnimTree.set("parameters/OSPose/request", AnimationNodeOneShot.ONE_SHOT_REQUEST_FIRE)
+	
+	owner.FloorNormal = Vector3.UP
 
 
 func Exit() -> void:
@@ -13,6 +15,11 @@ func Exit() -> void:
 
 
 func Update(_delta: float) -> void:
+	
+	
+	owner.CharMesh.AlignToY(owner.up_direction)
+	owner.up_direction = owner.up_direction.slerp(owner.FloorNormal.normalized(), _delta * owner.UP_VEC_LERP_RATE).normalized()
+	
 	if !owner.AnimTree.get("parameters/OSPose/active"):
 		ChangeState("Fall")
 		return
