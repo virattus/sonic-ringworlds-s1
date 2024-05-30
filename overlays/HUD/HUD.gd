c4da516a592985a1c56407019e1387a46ad66870
virattus <146156039+virattus@users.noreply.github.com>
2024-05-29 19:56:18 -0600

    some leftovers from the VRM import

--- overlays/HUD/HUD.gd
+++ overlays/HUD/HUD.gd
@@ -4,8 +4,11 @@ extends CanvasLayer
 @export var Sonic : Node3D
 
 
+@onready var lblSpeed = $LblSpeed
+
+
 func _process(_delta: float) -> void:
 	$StrikeDashMeterBG/StrikeDashMeter.scale.x = clamp(Sonic.DashModeCharge, 0.0, 1.0)
 	$StrikeDashMeterBG/LblStrikeDashPercentage.text = str(clamp(int(Sonic.DashModeCharge * 100.0), 0, 100)) + "%"
-	$LblSpeed.text = "Speed: " + str(roundf(Sonic.Speed)) 
+	lblSpeed.text = "Speed: " + str(roundf(Sonic.Speed)) 
 	$LblRings.text = "Rings\n" + str(Globals.RingCount)
