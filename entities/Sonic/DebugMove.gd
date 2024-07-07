05fee284edd6be9bdec88d73edc3752bf34e2831
virattus <146156039+virattus@users.noreply.github.com>
2024-07-06 18:04:38 -0600

    debug move axis aligned movement

--- entities/Sonic/DebugMove.gd
+++ entities/Sonic/DebugMove.gd
@@ -26,7 +26,15 @@ func Update(_delta: float) -> void:
 			"SubState": "Fall",
 		})
 	
-	var vel = owner.Controller.InputVelocity * MOVE_SPEED
+	var InputAnalogue = Input.get_vector("Move_Left", "Move_Right", "Move_Forward", "Move_Backward", 0.25)
+	
+	var CamBasis = get_viewport().get_camera_3d().get_camera_transform().basis
+	var CamForward = (CamBasis.z * Vector3(1, 0, 1)).normalized()
+	var CamRight = (CamBasis.x * Vector3(1, 0, 1)).normalized()
+	
+	
+	var vel = (InputAnalogue.x * CamRight) + (InputAnalogue.y * CamForward)
+	vel *= MOVE_SPEED
 	
 	if Input.is_action_pressed("DEBUG_AddHeight"):
 		vel.y += HEIGHT_SPEED * _delta
