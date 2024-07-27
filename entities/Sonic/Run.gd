e0db5838e40a18dcc95d459ea81812c50f670b56
virattus <146156039+virattus@users.noreply.github.com>
2024-07-27 12:26:09 -0600

    new mid speed run anim

--- entities/Sonic/Run.gd
+++ entities/Sonic/Run.gd
@@ -6,8 +6,7 @@ func Enter(_msg := {}) -> void:
 	owner.AnimTree.set("parameters/Movement/blend_amount", 0.0)
 	owner.AnimTree.set("parameters/Ground/blend_amount", 1.0)
 	owner.AnimTree.set("parameters/GroundSecondary/blend_amount", -1.0)
-	owner.AnimTree.set("parameters/Run/blend_amount", 0.0)
-	owner.AnimTree.set("parameters/TSRun/scale", owner.Speed * owner.PARAMETERS.MOVE_RUN_ANIM_SPEED_MODIFIER)
+	UpdateAnim()
 
 
 func Exit() -> void:
@@ -21,7 +20,8 @@ func Update(_delta: float) -> void:
 	owner.GroundCollision = owner.CollisionDetection(0, 0)
 	
 	owner.CharMesh.AlignToY(owner.up_direction)
-	owner.AnimTree.set("parameters/TSRun/scale", owner.Speed * owner.PARAMETERS.MOVE_RUN_ANIM_SPEED_MODIFIER)
+	
+	UpdateAnim()
 	
 	
 	if !owner.GroundCollision:
@@ -33,7 +33,7 @@ func Update(_delta: float) -> void:
 	
 	var inputVel = owner.GetInputVector()
 	
-	newVel += inputVel
+	newVel += inputVel * _delta
 	
 	if inputVel.length() >= 0.0 and owner.velocity.normalized() != Vector3.UP:
 		owner.CharMesh.look_at(owner.global_position + owner.velocity)
@@ -63,3 +63,12 @@ func Update(_delta: float) -> void:
 	if owner.Speed <= owner.PARAMETERS.MOVE_MIN_SPEED:
 		ChangeState("Idle")
 		return
+
+
+func UpdateAnim() -> void:
+	if owner.Speed > owner.PARAMETERS.RUN_MAX_SPEED:
+		owner.AnimTree.set("parameters/Run/blend_amount", 1.0)
+		owner.AnimTree.set("parameters/TSSprint/scale", owner.Speed * owner.PARAMETERS.MOVE_SPRINT_ANIM_SPEED_MODIFIER)
+	else:
+		owner.AnimTree.set("parameters/Run/blend_amount", 0.0)
+		owner.AnimTree.set("parameters/TSRun/scale", owner.Speed * owner.PARAMETERS.MOVE_RUN_ANIM_SPEED_MODIFIER)
