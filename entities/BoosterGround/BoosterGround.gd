87f264fbc4bb2ffabe94efd6732a108195d3c891
virattus <146156039+virattus@users.noreply.github.com>
2024-09-07 22:41:09 -0600

    booster still needs work

--- entities/BoosterGround/BoosterGround.gd
+++ entities/BoosterGround/BoosterGround.gd
@@ -6,6 +6,11 @@ extends Node3D
 
 
 func _on_area_3d_body_entered(body: Node3D) -> void:
+	$SndBooster.play()
+	body.SetVelocity(-global_transform.basis.z * BoostForce)
+	body.CharMesh.global_transform.basis = global_transform.basis
+	return
+	
 	body.StateM.ChangeState("Move", {
 		"IgnoreInput": BoostTime,
 		"Boost": -global_transform.basis.z * BoostForce,
