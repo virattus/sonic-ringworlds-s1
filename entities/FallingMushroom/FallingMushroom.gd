414693531425e62823e950b0acab3f521ab11dea
virattus <146156039+virattus@users.noreply.github.com>
2024-09-10 22:30:00 -0600

    falling mushroom

---
+++ entities/FallingMushroom/FallingMushroom.gd
@@ -0,0 +1,41 @@
+extends Area3D
+
+
+var Body = null
+var CapTween = null
+
+var Fall := false
+
+
+const FALL_SPEED = 10.0
+
+
+func _physics_process(delta: float) -> void:
+	if !Fall:
+		return
+	
+	global_position.y -= FALL_SPEED * delta
+
+
+func CapTweenDepressed() -> void:
+	$Timer.start()
+
+
+
+func _on_body_entered(body: Node3D) -> void:
+	Body = body
+	
+	CapTween = create_tween()
+	CapTween.tween_property($Cap, "position:y", -0.1, 0.1)
+	CapTween.tween_callback(CapTweenDepressed)
+
+
+func _on_body_exited(body: Node3D) -> void:
+	Body = null
+	
+	if $Cap.position.y != 0.0:
+		$Cap.position.y = 0.0
+
+
+func _on_timer_timeout() -> void:
+	Fall = true
